16c815cd518e26102d719e80c4aed09916934862
Pontus <pontus.jansson91@gmail.com>
2017-03-07 08:03:12 +0100

    MVP design for login and logged in

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -7,7 +8,19 @@ app.set('view engine', 'ejs');
 app.use('/assets/', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.render('index', {theUrl: req.url});
+    res.render('index');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', function(req, res){
+    res.send('Logging in');
+});
+
+app.get('/user', function(req, res){
+    res.render('user');
 });
 
 
